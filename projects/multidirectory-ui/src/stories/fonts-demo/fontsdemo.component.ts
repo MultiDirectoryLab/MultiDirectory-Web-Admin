4d7648a23196bd25f15adfc8b07a9eae5da16f1f
Oleg Brezhnev <oleg-423@yandex.ru>
2023-04-24 11:51:04 +0300

    added the inter font

---
+++ projects/multidirectory-ui/src/stories/fonts-demo/fontsdemo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-fontdemo',
+    templateUrl:  './fontsdemo.component.html',
+    styleUrls: ['./fontsdemo.component.scss']
+})
+export class FontsDemoComponent {
+}
---
+++ projects/multidirectory-ui/src/stories/fonts-demo/inter.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from "@storybook/angular"
+import { FontsDemoComponent } from "./fontsdemo.component"
+export default {
+    component: FontsDemoComponent,
+    title: 'Style/Fonts'
+} as Meta<FontsDemoComponent>
+
+
+export const Primary: StoryObj<FontsDemoComponent> = {
+    args: {
+    },
+  };
