f845a04cb6b8c2dc114489d64d080fca91059619
Oleg Brezhnev <oleg-423@yandex.ru>
2023-08-02 19:15:30 +0300

    update to angular@16

--- projects/multidirectory-app/src/app/core/setup/setup-route-guard.ts
+++ projects/multidirectory-app/src/app/core/setup/setup-route-guard.ts
@@ -1,11 +1,11 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable, map } from "rxjs";
 import { MultidirectoryApiService } from "../../services/multidirectory-api.service";
 import { Injectable } from "@angular/core";
 @Injectable({
     providedIn: 'root'
 })
-export class SetupRouteGuard implements CanActivate {
+export class SetupRouteGuard  {
     constructor(private multidirectoryApi: MultidirectoryApiService, private router: Router) {}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
         return this.multidirectoryApi.checkSetup().pipe(map(x => {
