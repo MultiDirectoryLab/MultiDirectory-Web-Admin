1411e383b9f759ab85c5fdc4757c1a98c5ae1e4c
Oleg Brezhnev <oleg-423@yandex.ru>
2023-08-02 11:05:33 +0300

    Added iconic view

--- projects/multidirectory-app/src/app/components/catalog-content/views/icon-view/icon-view.component.ts
+++ projects/multidirectory-app/src/app/components/catalog-content/views/icon-view/icon-view.component.ts
@@ -1,14 +1,31 @@
-import { AfterViewInit, Component } from "@angular/core";
+import { AfterViewInit, Component, forwardRef } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
+import { BaseViewComponent } from "../base-view.component";
+import { LdapNode } from "projects/multidirectory-app/src/app/core/ldap/ldap-loader";
 
 @Component({
     selector: 'app-icon-view',
     templateUrl: './icon-view.component.html',
-    styleUrls: ['./icon-view.component.scss']
+    styleUrls: ['./icon-view.component.scss'],
+    providers: [
+        { provide: BaseViewComponent, useExisting: forwardRef(() => IconViewComponent) }
+    ]
 })
-export class IconViewComponent implements AfterViewInit {
-    constructor(public toast: ToastrService) {}
+export class IconViewComponent extends BaseViewComponent implements AfterViewInit {
+    items: LdapNode[] = [];
+    constructor(public toast: ToastrService) {
+        super()
+    }
     ngAfterViewInit(): void {
         this.toast.info('Данный раздел находится в разразботке');
+    }
+
+    override setContent(items: LdapNode[], selectedNodes: LdapNode[]): void {
+        this.items = items;
+    }
+    override getSelected(): LdapNode[] {
+        return [];
+    }
+    override setSelected(selected: LdapNode[]): void {
     }
 }
