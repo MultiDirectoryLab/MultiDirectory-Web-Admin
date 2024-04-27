0172b68bc59947382272dcf22be1c198a10fc736
Oleg Brezhnev <oleg-423@yandex.ru>
2024-04-27 14:30:26 +0300

    linter

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: [
+          "tsconfig.*?.json",
+          "e2e/tsconfig.json"
+        ],
+        createDefaultProgram: true
+      },
+      extends: ["plugin:@angular-eslint/recommended"],
+      rules: {
+      }
+    },
+    {
+      files: ["*.component.html"],
+      extends: ["plugin:@angular-eslint/template/recommended"],
+      rules: {
+        "max-len": ["error", { "code": 140 }]
+      }
+    },
+    {
+      files: ["*.component.ts"],
+      extends: ["plugin:@angular-eslint/template/process-inline-templates"]
+    },
+    {
+      files: ['src/**/*.spec.ts', 'src/**/*.d.ts'],
+      parserOptions: {
+        project: './src/tsconfig.spec.json',
+      },
+      // Правила для линтера
+      extends: ['plugin:jasmine/recommended'],
+      // Плагин для запуска правил
+      plugins: ['jasmine'],
+      env: { jasmine: true },
+      // Отключим правило 'no-unused-vars'
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    }
+  ]
+}
