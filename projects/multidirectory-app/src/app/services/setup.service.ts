c3f180ab14283cf416ad059331411930d776ce40
Oleg Brezhnev <oleg-423@yandex.ru>
2025-02-10 10:49:20 +0300

    chore: setup tree 409 bypass

--- projects/multidirectory-app/src/app/services/setup.service.ts
+++ projects/multidirectory-app/src/app/services/setup.service.ts
@@ -1,28 +1,12 @@
 import { Injectable } from '@angular/core';
-import { SetupRequest } from '@models/setup/setup-request';
-import {
-  catchError,
-  delay,
-  EMPTY,
-  expand,
-  iif,
-  map,
-  Observable,
-  of,
-  retry,
-  switchMap,
-  take,
-  takeWhile,
-} from 'rxjs';
-import { MultidirectoryApiService } from './multidirectory-api.service';
-import { LoginService } from './login.service';
-import { KerberosTreeSetupRequest } from '@models/setup/kerberos-tree-setup-request';
-import { KerberosSetupRequest } from '@models/setup/kerberos-setup-request';
-import { ToastrService } from 'ngx-toastr';
 import { KerberosStatuses } from '@models/kerberos/kerberos-status';
+import { KerberosSetupRequest } from '@models/setup/kerberos-setup-request';
+import { KerberosTreeSetupRequest } from '@models/setup/kerberos-tree-setup-request';
+import { SetupRequest } from '@models/setup/setup-request';
+import { delay, expand, iif, map, Observable, of, switchMap, takeWhile } from 'rxjs';
 import { DnsApiService } from './dns-api.service';
-import { ReturnStatement } from '@angular/compiler';
-import { faL } from '@fortawesome/free-solid-svg-icons';
+import { LoginService } from './login.service';
+import { MultidirectoryApiService } from './multidirectory-api.service';
 
 @Injectable({
   providedIn: 'root',
@@ -32,7 +16,6 @@ export class SetupService {
     private api: MultidirectoryApiService,
     private dns: DnsApiService,
     private loginService: LoginService,
-    private toastr: ToastrService,
   ) {}
 
   setup(setupRequest: SetupRequest): Observable<boolean> {
@@ -47,51 +30,8 @@ export class SetupService {
           of(!!success),
         );
       }),
-      catchError((err) => {
-        return of(false);
-      }),
       switchMap((success) => {
-        return iif(
-          () => setupRequest.setupKdc,
-          this.api.kerberosTreeSetup(
-            new KerberosTreeSetupRequest({}).flll_from_setup_request(setupRequest),
-          ),
-          of(!!success),
-        );
-      }),
-      catchError((err) => {
-        if (err.status == 409) {
-          return of(true);
-        }
-        return of(false);
-      }),
-      switchMap((success) => {
-        return iif(
-          () => setupRequest.setupKdc,
-          this.api.kerberosSetup(
-            new KerberosSetupRequest({}).flll_from_setup_request(setupRequest),
-          ),
-          of(success),
-        );
-      }),
-      switchMap((success) => {
-        // Polling getKerberosStatus every 1 second until status === 1
-        let callCount = 0;
-        return iif(
-          () => setupRequest.setupKdc,
-          this.api.getKerberosStatus().pipe(
-            expand(() => {
-              callCount++;
-              if (callCount > 15) {
-                return EMPTY;
-              }
-              return this.api.getKerberosStatus().pipe(delay(1000));
-            }),
-            takeWhile((status) => status !== KerberosStatuses.READY, true),
-            map((x) => x == KerberosStatuses.READY), // Continue until status === 1
-          ),
-          of(success),
-        );
+        return iif(() => setupRequest.setupKdc, this.kerberosSetup(setupRequest), of(!!success));
       }),
     );
   }
@@ -100,12 +40,6 @@ export class SetupService {
     return this.api
       .kerberosTreeSetup(new KerberosTreeSetupRequest({}).flll_from_setup_request(setupRequest))
       .pipe(
-        catchError((err) => {
-          if (err.status == 409) {
-            return of(true);
-          }
-          return of(false);
-        }),
         switchMap((value) => {
           return this.api.kerberosSetup(
             new KerberosSetupRequest({}).flll_from_setup_request(setupRequest),
