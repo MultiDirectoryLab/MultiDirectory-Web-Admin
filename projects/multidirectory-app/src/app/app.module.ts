82058ed6e5f4dd3e44f442b5c327662a8ca06578
Dmitriy Smirnov <d.smirnov@multifactor.ru>
2025-04-14 11:36:40 +0300

    chore: remove unused module

--- projects/multidirectory-app/src/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Translation, TranslocoService } from '@jsverse/transloco';
-import { lastValueFrom } from 'rxjs';
-
-export function appInitializerFactory(translateService: TranslocoService) {
-  return (): Promise<Translation> => {
-    return lastValueFrom(translateService.load('ru-RU'));
-  };
-}
