b5fc20f8dd1dc4a816011fa69b4d4bd5158e27e9
Dmitriy Smirnov <d.smirnov@multifactor.ru>
2025-05-14 09:09:24 +0300

    feat(dns-settings): add redirection zones tab

--- projects/multidirectory-app/src/app/features/dns/dns-settings.component.ts
+++ projects/multidirectory-app/src/app/features/dns/dns-settings.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, inject, DestroyRef } from '@angular/core';
+import { Component, OnInit, inject, DestroyRef, signal } from '@angular/core';
 import { DnsRuleListItemComponent } from '@features/dns/dns-rule-list-item/dns-rule-list-item.component';
+import { RedirectZonesRow } from '@features/dns/interfaces/redirect-zones.interface';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { translate, TranslocoPipe } from '@jsverse/transloco';
@@ -12,7 +13,16 @@ import { DnsStatuses } from '@models/dns/dns-statuses';
 import { AppSettingsService } from '@services/app-settings.service';
 import { AppWindowsService } from '@services/app-windows.service';
 import { DnsApiService } from '@services/dns-api.service';
-import { AlertComponent, ButtonComponent } from 'multidirectory-ui-kit';
+import {
+  AlertComponent,
+  ButtonComponent,
+  TabPaneComponent,
+  TabComponent,
+  TabDirective,
+  DatagridComponent,
+  Page,
+} from 'multidirectory-ui-kit';
+import { TableColumn } from 'ngx-datatable-gimefork';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, EMPTY, switchMap, take } from 'rxjs';
 import { DialogService } from '../../components/modals/services/dialog.service';
@@ -28,6 +38,8 @@ import {
 } from '../../components/modals/interfaces/dns-rule-dialog.interface';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
+export const MOCK_REDIRECTION_ZONES = [];
+
 @Component({
   selector: 'app-dns-settings',
   templateUrl: './dns-settings.component.html',
@@ -38,6 +50,10 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     DnsRuleListItemComponent,
     AlertComponent,
     FaIconComponent,
+    TabPaneComponent,
+    TabComponent,
+    TabDirective,
+    DatagridComponent,
   ],
 })
 export class DnsSettingsComponent implements OnInit {
@@ -45,6 +61,31 @@ export class DnsSettingsComponent implements OnInit {
   public dnsStatus = new DnsStatusResponse({});
   public rules: DnsRule[] = [];
   public faCircleExclamation = faCircleExclamation;
+
+  public redirectionZonesColumns: TableColumn[] = [
+    { name: 'Имя зоны', prop: 'zoneName', flexGrow: 1, checkboxable: true },
+    { name: 'Состояние', prop: 'status', flexGrow: 1 },
+    { name: 'Перенаправление зон', prop: 'redirection', flexGrow: 1 },
+  ];
+  public redirectionZonesRows: RedirectZonesRow[] = [
+    {
+      zoneName: 'Зона 1',
+      status: 'Включена',
+      redirection: 'example.com',
+    },
+    {
+      zoneName: 'Зона 2',
+      status: 'Выключена',
+      redirection: '1.2.3.4',
+    },
+    {
+      zoneName: 'Зона 3',
+      status: 'Включена',
+      redirection: '192.168.1.10',
+    },
+  ];
+  public redirectionZonesPage = new Page();
+  private selectedRedirectionZones = signal<RedirectZonesRow[]>([]);
 
   private dialogService: DialogService = inject(DialogService);
   private dnsService: DnsApiService = inject(DnsApiService);
@@ -194,5 +235,36 @@ export class DnsSettingsComponent implements OnInit {
     const result = new DnsRule(rule);
     result.record_name = result.record_name.replace('.' + this.dnsStatus.zone_name, '');
     return result;
+  }
+
+  public onTabChanged() {
+    console.log('tab changed');
+  }
+
+  public onAddRedirectionZone() {
+    this.toastr.info('Add redirection zone functionality will be implemented in the future');
+  }
+
+  public onEnableRedirectionZone() {
+    this.toastr.info('Enable redirection zone functionality will be implemented in the future');
+  }
+
+  public onDisableRedirectionZone() {
+    this.toastr.info('Disable redirection zone functionality will be implemented in the future');
+  }
+
+  public onDeleteRedirectionZone() {
+    const selectedRows = this.selectedRedirectionZones();
+
+    if (!selectedRows.length) {
+      this.toastr.error(translate('dns-settings.should-select-zones'));
+      return;
+    }
+
+    this.toastr.info('Delete redirection zone functionality will be implemented in the future');
+  }
+
+  public onRedirectionZonesSelectionChanged(rows: RedirectZonesRow[]) {
+    this.selectedRedirectionZones.set(rows);
   }
 }
