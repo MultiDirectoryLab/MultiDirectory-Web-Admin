6c8b7b4a1464e21ea491ba12c4b9796e006c6adc
Oleg Brezhnev <oleg-423@yandex.ru>
2025-06-24 07:27:28 +0300

    chore: update memberof component

--- projects/multidirectory-app/src/app/features/ldap-properties/member-of/member-of.component.ts
+++ projects/multidirectory-app/src/app/features/ldap-properties/member-of/member-of.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, viewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, Input, viewChild } from '@angular/core';
 import { Constants } from '@core/constants';
 import { ENTITY_TYPES } from '@core/entities/entities-available-types';
 import { Group } from '@core/groups/group';
@@ -6,13 +6,14 @@ import { LdapAttributes } from '@core/ldap/ldap-attributes/ldap-attributes';
 import { translate, TranslocoPipe } from '@jsverse/transloco';
 import { AppNavigationService } from '@services/app-navigation.service';
 import { ButtonComponent, DatagridComponent } from 'multidirectory-ui-kit';
-import { take } from 'rxjs';
+import { from, take } from 'rxjs';
 import { EntitySelectorDialogComponent } from '../../../components/modals/components/dialogs/entity-selector-dialog/entity-selector-dialog.component';
 import {
   EntitySelectorDialogData,
   EntitySelectorDialogReturnData,
 } from '../../../components/modals/interfaces/entity-selector-dialog.interface';
 import { DialogService } from '../../../components/modals/services/dialog.service';
+import { LdapTreeviewService } from '@services/ldap/ldap-treeview.service';
 
 @Component({
   selector: 'app-member-of',
@@ -22,7 +23,8 @@ import { DialogService } from '../../../components/modals/services/dialog.servic
 })
 export class MemberOfComponent {
   private dialogService: DialogService = inject(DialogService);
-  private navigation: AppNavigationService = inject(AppNavigationService);
+  private ldapTreeview: LdapTreeviewService = inject(LdapTreeviewService);
+  private cdr = inject(ChangeDetectorRef);
   readonly groupList = viewChild<DatagridComponent>('groupList');
   public groups: Group[] = [];
   public columns = [
@@ -47,32 +49,36 @@ export class MemberOfComponent {
 
   public addGroup() {
     const types = ['group', 'user'];
-
-    this.dialogService
-      .open<
-        EntitySelectorDialogReturnData,
-        EntitySelectorDialogData,
-        EntitySelectorDialogComponent
-      >({
-        component: EntitySelectorDialogComponent,
-        dialogConfig: {
-          data: {
-            selectedEntities: [],
-            selectedEntityTypes: ENTITY_TYPES.filter((x) => types.includes(x.entity)) ?? [],
-            allowSelectEntityTypes: false,
-            entityToMove: [],
-            selectedPlaceDn: '',
-          },
-        },
-      })
-      .closed.pipe(take(1))
-      .subscribe((res) => {
-        if (res && !!this.accessor) {
-          res = res.filter((x) => !this.accessor!.memberOf?.includes(x.id)) ?? res;
-          this.accessor.memberOf =
-            this.accessor?.memberOf?.concat(res.map((x) => x.id)) ?? res.map((x) => x.id);
-          this.groups = this.accessor?.memberOf?.map((x) => this.createGroupFromDn(x)) ?? [];
-        }
+    from(this.ldapTreeview.load(''))
+      .pipe(take(1))
+      .subscribe((ldapTree) => {
+        this.dialogService
+          .open<
+            EntitySelectorDialogReturnData,
+            EntitySelectorDialogData,
+            EntitySelectorDialogComponent
+          >({
+            component: EntitySelectorDialogComponent,
+            dialogConfig: {
+              data: {
+                selectedEntities: [],
+                selectedEntityTypes: ENTITY_TYPES.filter((x) => types.includes(x.entity)) ?? [],
+                allowSelectEntityTypes: false,
+                entityToMove: [],
+                selectedPlaceDn: ldapTree[0].id,
+              },
+            },
+          })
+          .closed.pipe(take(1))
+          .subscribe((res) => {
+            if (res && !!this.accessor) {
+              res = res.filter((x) => !this.accessor!.memberOf?.includes(x.id)) ?? res;
+              this.accessor.memberOf =
+                this.accessor?.memberOf?.concat(res.map((x) => x.id)) ?? res.map((x) => x.id);
+              this.groups = this.accessor?.memberOf?.map((x) => this.createGroupFromDn(x)) ?? [];
+              this.cdr.detectChanges();
+            }
+          });
       });
   }
 
