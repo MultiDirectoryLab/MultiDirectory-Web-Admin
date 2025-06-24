ee371b608a07948a89057a3caabd511298fd97ec
Oleg Brezhnev <oleg-423@yandex.ru>
2025-06-24 14:21:23 +0300

    chore: entity-browser in progress

--- projects/multidirectory-app/src/app/features/schema-browser/components/entities-browser/entities-browser.component.ts
+++ projects/multidirectory-app/src/app/features/schema-browser/components/entities-browser/entities-browser.component.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { SchemaEntity } from '@models/api/schema/entities/schema-entity';
 import { SchemaService } from '@services/schema/schema.service';
+import { MultidirectoryUiKitModule } from '../../../../../../../multidirectory-ui-kit/src/lib/multidirectory-ui-kit.module';
+import { TableColumn } from 'ngx-datatable-gimefork';
+import { translate } from '@jsverse/transloco';
 
 @Component({
-  selector: 'app-entities-browser',
-  imports: [CommonModule],
+  imports: [CommonModule, MultidirectoryUiKitModule],
   templateUrl: './entities-browser.component.html',
   styleUrl: './entities-browser.component.scss',
 })
@@ -13,7 +15,10 @@ export class EntitiesBrowserComponent implements OnInit {
   private schema = inject(SchemaService);
 
   entites = signal<SchemaEntity[]>([]);
-
+  columns = signal<TableColumn[]>([
+    { name: translate('schema-entities-browser.column-name'), prop: 'name' },
+    { name: translate('schema-entities-browser.column-type'), prop: 'type' },
+  ]);
   ngOnInit(): void {
     this.schema.getEntities().subscribe((result) => {
       this.entites.set(result);
